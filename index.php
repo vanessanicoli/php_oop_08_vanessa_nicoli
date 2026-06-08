079cf7e88c9cdd12336747ad0d101a4b0068568b
Vanessa Nicolì <nicoli.vanessa1996@gmail.com>
2026-06-08 20:07:21 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+// Crea un trait chiamato “Calculator” definendo le seguenti operazioni tra numeri:
+
+//      public function sum($a, $b) {
+//          return $a + $b;
+//      }
+
+//      public function sub($a, $b) {
+//          return $a - $b;
+//      }
+
+//      public function mul($a, $b) {
+//          return $a * $b;
+//      }
+
+//      public function div($a, $b) {
+//          return $a / $b;
+//      }
+
+//      public function sqr($a){
+//          return sqrt($numero);
+//      }
+
+// Crea quindi una classe Rettangolo con i seguenti attributi:
+// -	Base (b);
+// -	Altezza (h);
+
+// Il tuo compito sarà quello di creare 3 metodi che andranno a calcolare:
+// -	Area → b * h
+// -	Perimetro → 2 * b + 2 * h
+// -	Diagonale → √ hˆ2 + bˆ2 (Tutto sotto radice)
+
+// Tutte queste operazioni però dovranno essere richiamate dal trait Calculator
